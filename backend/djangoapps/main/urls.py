e6542d5e879897fc176974e27f1355c3de2d2c1f
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-26 02:43:11 +0300

    Migrating from master/slave terminology

--- backend/djangoapps/main/urls.py
+++ backend/djangoapps/main/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 
-from .views import IndexView, WebMasterView
+from .views import IndexView, WebMainView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
 
-    # for google webmaster
+    # for google webmain
     url(r'google4c82de08f55a8973.html',
-        WebMasterView.as_view(), name='googleWebMaster'),
+        WebMainView.as_view(), name='googleWebMain'),
 ]
